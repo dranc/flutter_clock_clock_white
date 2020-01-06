75ee1b6843f7325a423a3ae775bb75adda95005d
Dimitri Ranc <ranc.dimitri@gmail.com>
2020-01-06 22:08:28 +0100

    add margin to clock

--- clock_clock/lib/clock_clock.dart
+++ clock_clock/lib/clock_clock.dart
@@ -19,6 +19,7 @@ class _ClockClockState extends State<ClockClock> {
 
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.all(20.0),
       child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
